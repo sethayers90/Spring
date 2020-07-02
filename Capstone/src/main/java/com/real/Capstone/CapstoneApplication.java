fa99d935eb651d93433fd1f4dc96305712eacd86
SETH R AYERS <sethayers90@gmail.com>
2020-07-02 17:27:48 -0400

    Add files via upload

---
+++ Capstone/src/main/java/com/real/Capstone/CapstoneApplication.java
@@ -0,0 +1,16 @@
+package com.real.Capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan("")
+public class CapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneApplication.class, args);
+		
+	}
+
+}
